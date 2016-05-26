f2ed0d9986c4a1419a6239f66dd47a9ec2ab7ed8
Rosales Calle,Rosario <1000104644.rr@gmail.com>
2016-05-26 15:28:12 -0500

    Oh!

--- index.js
+++ index.js
@@ -7,15 +7,22 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/public/index.html');
 });
 
+app.use( express.static('public'));
 
 io.on('connection', function(socket){
 	console.log('Un nuevo miembro');
+
 
 	socket.on('chat', function(_msg){
 		io.emit('nuevo_mensaje', _msg);
 	});
 
+	socket.on('conectado',function(_int){
+		io.emit('nuevo_integrante', _int);
+	}); 
+
 });
+
 
 http.listen(8080, function () {
 	console.log('Iniciando sesión');
